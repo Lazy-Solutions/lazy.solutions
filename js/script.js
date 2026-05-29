458d1407086293daaed5f633f49aa153f23fe2c3
Patric Keisala <finne_no1@hotmail.com>
2026-05-29 23:44:06 +0200

    Website overhaul

--- js/script.js
+++ js/script.js
@@ -1,12 +1,21 @@
-var viewportWidth = document.documentElement.clientWidth;
+document.addEventListener('DOMContentLoaded', function () {
+    const nav = document.getElementById('navigation');
+    const navLinks = document.querySelector('.nav-links');
+    const menuToggle = document.querySelector('.menu-toggle');
 
-$(document).ready(function () {
-    window.onscroll = function () {
-        if (window.pageYOffset <= 550) {
-            $(".lazy_logo").addClass("hidden");
-        }
-        else {
-            $(".lazy_logo").removeClass("hidden");
-        }
+    if (menuToggle) {
+        menuToggle.addEventListener('click', function () {
+            nav.classList.toggle('nav-open');
+            document.body.classList.toggle('no-scroll');
+        });
     }
+
+    // Close menu when a link is clicked
+    const links = document.querySelectorAll('.nav-links a');
+    links.forEach(link => {
+        link.addEventListener('click', () => {
+            nav.classList.remove('nav-open');
+            document.body.classList.remove('no-scroll');
+        });
+    });
 });
